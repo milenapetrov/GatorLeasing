be2b4bcb61c9c4e9416a902f6d1142917d912af7
Milena Petrovic <92889847+milenapetrov@users.noreply.github.com>
2023-02-06 17:55:59 -0500

    button stuff

--- gator-leasing-client/src/app/post/post.component.ts
+++ gator-leasing-client/src/app/post/post.component.ts
@@ -4,20 +4,27 @@ import { PostLeaseRequest } from '../models/PostLeaseRequest';
 @Component({
   selector: 'app-post',
   template: `
-  <h2> {{ post.name }} </h2>
-  <div> 
+    <div> 
       <label for="name"> Post name: </label>
-      <input id="name" [(ngModel)]="post.name" placeholder="name">
-   </div>
-      <br>
-      <br>
-      <div class="box">
-       <br>
-        1 bedroom in a 4x4 at the Standard.
-        <br>
-        <br> 
-        <br>
-      </div>
+      <input #p
+            (keyup.enter)= "addPost(p.value)"
+            
+            placeholder="name">
+
+      <button type="button" (click)="addPost(p.value)"> post </button>
+    </div>
+  
+
+    <br>
+    <br>
+    <div class="box">
+    <br>
+      {{post.name}}
+    <br>
+    <br> 
+    <br>
+    </div>
+  
   `
   //templateUrl: './post.component.html',
   ,styleUrls: ['./post.component.css']
@@ -25,8 +32,14 @@ import { PostLeaseRequest } from '../models/PostLeaseRequest';
 export class PostComponent {
   //name: 'Ur moms Penthouse'
   post: PostLeaseRequest ={
-    name: 'urmom'
+    name: ' '
+  };
+
+  addPost(name: string) {
+    this.post.name = name;
   };
 }
 
 
+//<input id="name" [(ngModel)]="post.name" placeholder="name">         for dynamic input
+//(blur)= "addPost(p.value); p.value='' "
