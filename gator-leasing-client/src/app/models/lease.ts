93d6a64b01add4bc980e2e521e816bad6de1afd9
Jacob Immich <jacobimmich@gmail.com>
2023-03-29 18:37:28 -0400

    Update Lease Model

--- gator-leasing-client/src/app/models/lease.ts
+++ gator-leasing-client/src/app/models/lease.ts
@@ -1,5 +1,24 @@
+import { NumberMatcher } from "cypress/types/net-stubbing";
+import {Address} from './address'
+
 export interface Lease{
     id: number,
     name: string,
-    
+    ownerID: number,
+    address: Address,
+    startDate: Date,
+    endDate: Date,
+    rent: number,
+    utilites: number,
+    parkingCost: number,
+    totalCost: number,
+    squareFootage: number,
+    furnished: boolean,
+    parking: boolean,
+    beds: number,
+    baths: number,
+    amenities: string,
+    appliances: string,
+    description: string,
+    contacts: object,
 }
