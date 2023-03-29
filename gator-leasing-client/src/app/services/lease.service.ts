a846a5f0e8a6425a7597a2302e67986d9296b681
Jacob Immich <jacobimmich@gmail.com>
2023-03-29 18:56:20 -0400

    Add baseurl, leaseurl to lease service

--- gator-leasing-client/src/app/services/lease.service.ts
+++ gator-leasing-client/src/app/services/lease.service.ts
@@ -8,19 +8,22 @@ import { Post } from '../models/post'
   providedIn: 'root'
 })
 export class LeaseService {
-  constructor(private http : HttpClient) { }
+  constructor(private http : HttpClient) {}
+
+  BASEURL: string = 'http://localhost:8080'
+  LEASEURL: string = '/leases'
 
   getLeases(): Observable<Lease[]> {
     console.log("get leases")
-    return this.http.get<Lease[]>("http://localhost:8080/leases");
+    return this.http.get<Lease[]>(`${this.BASEURL}${this.LEASEURL}`);
   }
 
   createPost(post: Post){
-    this.http.post<Post>("http://localhost:8080/leases", post).subscribe(response =>{ 
+    this.http.post<Post>(`${this.BASEURL}${this.LEASEURL}`, post).subscribe(response =>{ 
       console.log(response);  })  
   }
 
   updatePost(post: Post): Observable<any> {
-    return this.http.put("http://localhost:8080/leases", post);
+    return this.http.put(`${this.BASEURL}${this.LEASEURL}`, post);
   }
 }
