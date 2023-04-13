6b0208d677b160f3e44a35ccdaa306d9012cfebb
nicklasRice <nicklas.rice@ufl.edu>
2023-04-13 10:14:54 -0400

    room number shouldn't be required

--- gator-leasing-server/view-model/address.go
+++ gator-leasing-server/view-model/address.go
@@ -2,7 +2,7 @@ package viewModel
 
 type Address struct {
 	Street     string `json:"street" validate:"required"`
-	RoomNumber string `json:"roomNumber" validate:"required"`
+	RoomNumber string `json:"roomNumber"`
 	City       string `json:"city" validate:"required"`
 	State      string `json:"state" validate:"required"`
 	ZipCode    string `json:"zipCode" validate:"required"`
