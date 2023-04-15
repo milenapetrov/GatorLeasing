8a5d84a838fb56fe39bc6ba451d4cb9c95229716
Jacob Immich <jacobimmich@gmail.com>
2023-04-15 17:29:16 -0400

    Change "list" to "get"

--- gator-leasing-server/handler/lease_handler.go
+++ gator-leasing-server/handler/lease_handler.go
@@ -352,7 +352,7 @@ func (h *LeaseHandler) GetPaginatedLeases(w http.ResponseWriter, r *http.Request
 
 // swagger:route GET /myleases leases GetMyLeases
 //
-// List my leases.
+// Get my leases.
 //
 // get all my leases.
 //
